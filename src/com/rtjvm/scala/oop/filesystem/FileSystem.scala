798ea0f395713118a828b169c1d9f4e612d70e00
nargok <nargok@gmail.com>
2019-09-21 19:19:46 +0900

    create filesytem object

---
+++ src/com/rtjvm/scala/oop/filesystem/FileSystem.scala
@@ -0,0 +1,12 @@
+package com.rtjvm.scala.oop.filesystem
+
+import java.util.Scanner
+
+object FileSystem extends App {
+
+  val scanner = new Scanner(System.in)
+
+  while (true) {
+    print("$ ")
+  }
+}
