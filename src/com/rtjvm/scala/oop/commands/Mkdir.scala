dddc2b1e3268cc8ea875a03bdc21ec45d27c1d49
nargok <nargok@gmail.com>
2019-09-22 10:45:06 +0900

    error handling for mkdir incomplete case

---
+++ src/com/rtjvm/scala/oop/commands/Mkdir.scala
@@ -0,0 +1,8 @@
+package com.rtjvm.scala.oop.commands
+import com.rtjvm.scala.oop.filesystem.State
+
+class Mkdir(name: String) extends Command {
+
+  override def apply(state: State): State = ???
+
+}
